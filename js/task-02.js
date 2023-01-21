0c79081fcc768cdbd95307af1ec6ed9c4702b019
Jose Gonzales <jose.gonzales08@ymail.com>
2023-01-21 16:24:53 -0500

    First task-02 push

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const emptyUl = document.querySelectorAll('#ingredients');
+
+ingredients.forEach(i => {
+    i = document.createElement('li');
+    i.textContent = "${i}";
+    i.classList.add("item");
+    emptyUl[0].append(i);
+});
+
+console.log(emptyUl);
+
+// Write a script that, for each element in the ingredients array:
+            // forEach Loop
+// Will create a separate <li> element. 
+// Be sure to use the document.createElement() method.
+            // document.createElement(), .appendChild()
+// Will add the ingredient name as its text content.
+            // elem.innerHTML
+// Will add the item class to the element.
+            // elem.classList.add(cls)
+// Then will insert all <li> to the ul#ingredients 
+// list in a single operation.
