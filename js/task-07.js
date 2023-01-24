4d240971920c276da0e36770a291c7e500573d99
Jose Gonzales <jose.gonzales08@ymail.com>
2023-01-24 14:43:41 -0500

    task 7 push

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const content = document.querySelector('#text');
+const controller = document.querySelector('#font-size-control');
+
+controller.addEventListener("input", (e));
+
+function e() {
+        const sliderValue = controller.value;
+        content.style.fontSize = sliderValue + "px";
+    };
+
+
+// Write a script that responds to changes in the value 
+// of input#font-size-control (input event) and changes 
+// the inline style of span#text by updating the 
+// font-size property. As a result, the text size will 
+// change responding to scrollbar dragging.
